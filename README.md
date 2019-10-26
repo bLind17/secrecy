288dffd36e6b042389cb51b0da5489f285db8631
Lukas Lipp <lukas@lipp.net>
2019-10-26 18:53:22 +0200

    removed unsued express reference, updated readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# secrecy
+
+## web
+Standalone js web client.
+
+## node.js
+To use the node, you must install ws:
+```bash
+	npm install ws
+```
+
+And put the secrecy modules into the node_modules folder!
--- node/secrecy.js
+++ node/secrecy.js
@@ -1,37 +1,8 @@
-const express = require('express');
 const path = require('path');
 
 const secrecyServer = require('secrecy_server');
 const WebSocket = require('ws');
 
-/*
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-const name = 'secrecy_node';
-const port = '1337';
-
-app.set('views', path.join(__dirname, 'pug'));
-app.set('view engine', 'pug');
-app.use('/games/secrecy/api', function (req, res) {
-	secrecyServer.handleApiRequest(req, res);
-});
-
-app.use('/games/secrecy/js', express.static(path.join(__dirname, 'js')));
-
-app.get('/games/secrecy/host', function (req, res) {
-	secrecyServer.handleHostRequest(req, res);
-});
-
-app.get('/games/secrecy/play', function (req, res) {
-	secrecyServer.handlePlayRequest(req, res);
-});
-
-app.get('/games/secrecy', function (req, res) {
-	secrecyServer.handleRequest(req, res);
-});
-*/
 function noop() {}
 
 function heartbeat() {
@@ -47,8 +18,8 @@ wss.on('connection', function connection(ws) {
   ws.on('pong', heartbeat);
 });
 
+// keep clients alive (webhost requires this)
 const interval = setInterval(function ping() {
-	//console.log("ping");
 	wss.clients.forEach(function each(ws) {
 		if (ws.isAlive === false) return ws.terminate();
 
@@ -58,14 +29,3 @@ const interval = setInterval(function ping() {
 }, 30000);
 
 console.log("Websockets running.");
-
-/*
-io.on('connection', ws => {
-	secrecyServer.handleSocketClient(ws);
-	console.log(ws._socket.remoteAddress + " connected."); 
-})
-
-server.listen(port, '0.0.0.0', () => {
-	console.log(`${name} is listening on port ${port}`);
-});
-*/
